ad32c0023074ed104c0c8a770bd9a4c423f9e360
LE THAI VINH <lthvinhhoctap@gmail.com>
2025-11-18 19:20:37 +0700

    Updated PolygonSVG
    Bổ sung PolygonSVG

---
+++ SVGReader/PolygonSVG.h
@@ -0,0 +1,20 @@
+#ifndef POLYGONSVG_H
+#define POLYGONSVG_H
+#include "Function.h"
+#include "FillShapeSVG.h"
+
+using namespace Gdiplus;
+using namespace std;
+using namespace rapidxml;
+
+class PolygonSVG : public FillShapeSVG {
+private:
+	string points;
+
+public:
+	PolygonSVG();
+	void read(xml_node<>* node);
+	void draw(Graphics& graphics) override;
+	~PolygonSVG();
+};
+#endif POLYGONSVG_H
