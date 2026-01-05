ec000799d8ae4eba9003998c1a124514d9be14e4
TRAN LE XUAN TAN <tranlexuantan2k6@gmail.com>
2026-01-05 21:21:16 +0700

    Update LineSVG.cpp
    Update LineSVG.cpp

--- SVGReader/LineSVG.cpp
+++ SVGReader/LineSVG.cpp
@@ -1,6 +1,6 @@
-#pragma once
 #include "LineSVG.h"
-#include "DefinitionsSVG.h" // Thêm include để nhận kiểu dữ liệu
+#include "DefinitionsSVG.h"
+#include <iostream>
 
 using namespace Gdiplus;
 using namespace std;
@@ -13,54 +13,98 @@ LineSVG::LineSVG() {
 }
 
 void LineSVG::read(xml_node<>* node) {
-    for (xml_attribute<>* attr = node->first_attribute(); attr; attr = attr->next_attribute()) {
-        string type(attr->name());
-        if (type == "x1") 
-            x1 = stof(attr->value());
-        else if (type == "y1") 
-            y1 = stof(attr->value());
-        else if (type == "x2") 
-            x2 = stof(attr->value());
-        else if (type == "y2") 
-            y2 = stof(attr->value());
-        else if (type == "opacity") { 
-            opacity.setOpacity(stof(attr->value())); 
-            hasOpacity = true; 
+    hasStrokeColor = false;
+    hasStrokeWidth = false;
+    hasStrokeOpacity = false;
+    hasOpacity = false;
+    className = "";
+
+    xml_attribute<>* classAttribute = node->first_attribute("class");
+    if (classAttribute) {
+        className = classAttribute->value();
+    }
+
+    xml_attribute<>* styleAttribute = node->first_attribute("style");
+    string inlineStyle = styleAttribute ? styleAttribute->value() : "";
+
+    for (xml_attribute<>* attribute = node->first_attribute(); attribute; attribute = attribute->next_attribute()) {
+        string attributeName(attribute->name());
+        if (attributeName == "x1") {
+            x1 = stof(attribute->value());
         }
-        else if (type == "stroke") { 
-            stroke.setStrokeColor(getRGB(attr->value())); 
-            hasStrokeColor = true; 
+        else if (attributeName == "y1") {
+            y1 = stof(attribute->value());
         }
-        else if (type == "stroke-width") { 
-            stroke.setStrokeWidth(stof(attr->value())); 
-            hasStrokeWidth = true; 
+        else if (attributeName == "x2") {
+            x2 = stof(attribute->value());
         }
-        else if (type == "stroke-opacity") { 
-            stroke.setStrokeOpacity(stof(attr->value())); 
-            hasStrokeOpacity = true; 
+        else if (attributeName == "y2") {
+            y2 = stof(attribute->value());
         }
-        else if (type == "transform") 
-            parseTransform(attr->value());
+        else if (attributeName == "opacity") {
+            opacity.setOpacity(stof(attribute->value()));
+            hasOpacity = true;
+        }
+        else if (attributeName == "stroke") {
+            stroke.setStrokeColor(getRGB(attribute->value()));
+            hasStrokeColor = true;
+        }
+        else if (attributeName == "stroke-width") {
+            stroke.setStrokeWidth(stof(attribute->value()));
+            hasStrokeWidth = true;
+        }
+        else if (attributeName == "stroke-opacity") {
+            stroke.setStrokeOpacity(stof(attribute->value()));
+            hasStrokeOpacity = true;
+        }
+        else if (attributeName == "transform") {
+            parseTransform(attribute->value());
+        }
+    }
+
+    if (!inlineStyle.empty()) {
+        FillSVG unusedFill;
+        bool tempFill = false, tempFillOpacity = false;
+        string tempGradientId;
+        ElementSVG::parseStyle(inlineStyle, unusedFill, stroke, opacity, tempFill, tempFillOpacity, hasStrokeColor, hasStrokeWidth, hasStrokeOpacity, tempGradientId);
     }
 }
 
 void LineSVG::draw(Graphics& graphics, const DefinitionsSVG& defs) {
-    GraphicsState state = graphics.Save();
+    GraphicsState graphicsState = graphics.Save();
     graphics.MultiplyTransform(&transformMatrix, MatrixOrderPrepend);
     graphics.SetSmoothingMode(SmoothingModeAntiAlias);
 
-    float opacityAll = ((float)opacity.getOpacity() * 255);
+    if (!className.empty()) {
+        string styleString = defs.getStyleByClass(className);
+        FillSVG unusedFill;
+        StrokeSVG tempStroke = stroke;
+        OpacitySVG tempOpacity = opacity;
+        bool tempFill = false, tempFillOpacity = false, tempHasStroke = false, tempHasStrokeWidth = false, tempHasStrokeOpacity = false;
+        string tempGradientId = "";
+        ElementSVG::parseStyle(styleString, unusedFill, tempStroke, tempOpacity, tempFill, tempFillOpacity, tempHasStroke, tempHasStrokeWidth, tempHasStrokeOpacity, tempGradientId);
+
+        if (!hasStrokeColor && tempHasStroke) {
+            stroke.setStrokeColor(tempStroke.getStrokeColor());
+        }
+        if (!hasStrokeWidth && tempHasStrokeWidth) {
+            stroke.setStrokeWidth(tempStroke.getStrokeWidth());
+        }
+        if (!hasStrokeOpacity && tempHasStrokeOpacity) {
+            stroke.setStrokeOpacity(tempStroke.getStrokeOpacity());
+        }
+    }
+
+    float opacityAll = opacity.getOpacity() * 255.0f;
     Color rgbStroke = stroke.getStrokeColor();
-    float opacityStroke = minValue(opacityAll, ((float)stroke.getStrokeOpacity() * 255));
-    if (stroke.getStrokeColor().GetA() == 0) opacityStroke = 0;
+    if (rgbStroke.GetA() > 0 && stroke.getStrokeWidth() > 0) {
+        float finalAlpha = opacityAll * stroke.getStrokeOpacity();
+        Color finalStroke((BYTE)finalAlpha, rgbStroke.GetR(), rgbStroke.GetG(), rgbStroke.GetB());
+        Pen penStroke(finalStroke, stroke.getStrokeWidth());
+        graphics.DrawLine(&penStroke, x1, y1, x2, y2);
+    }
 
-    rgbStroke = Color(opacityStroke, rgbStroke.GetR(), rgbStroke.GetG(), rgbStroke.GetB());
-    Pen penStroke(rgbStroke, stroke.getStrokeWidth());
-
-    if (stroke.getStrokeColor().GetA() > 0 || stroke.getStrokeColor().GetR() > 0 || stroke.getStrokeColor().GetG() > 0 || stroke.getStrokeColor().GetB() > 0)
-        graphics.DrawLine(&penStroke, this->x1, this->y1, this->x2, this->y2);
-
-    graphics.Restore(state);
+    graphics.Restore(graphicsState);
 }
 
 LineSVG::~LineSVG() {}
